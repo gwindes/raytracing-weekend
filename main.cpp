4cffe2b0c1af205d6b0074208ca42aa16d045228
Grant Windes <gwindes@gmail.com>
2020-05-22 20:57:17 -0700

    CH7 - Basic diffuse material and gamma correction

--- main.cpp
+++ main.cpp
@@ -10,10 +10,20 @@
 #define MAXFLOAT 9999999.9
 
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+
+    return p;
+}
+
 vec3 color(const ray& r, hitable* world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color( ray(rec.p, target-rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
@@ -48,8 +58,10 @@ int main() {
                 col += color(r, world);
             }
 
+            // average the pixel value for ns rays casted
             col /= float(ns);
-
+            // gamma correction
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
