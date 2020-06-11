9e8e04399b57fde40b02af7a39e98944830eb8d0
Grant Windes <gwindes@gmail.com>
2020-06-10 19:27:54 -0700

    Additional cleanup removing unused cpp files that only require header file. Moved lambertian implementation to cpp

--- lambertian.cpp
+++ lambertian.cpp
@@ -1,1 +1,9 @@
 #include "lambertian.h"
+
+
+bool lambertian::scatter(const ray& r_in, const hit_record rec, vec3& attenuation, ray& scattered) const {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    scattered = ray(rec.p, target - rec.p);
+    attenuation = albedo;
+    return true;
+}
--- lambertian.h
+++ lambertian.h
@@ -1,20 +1,16 @@
 #ifndef RAYTRACER_WEEKEND_LAMBERTIAN_H
 #define RAYTRACER_WEEKEND_LAMBERTIAN_H
 
+#include "material.h"
+#include "hitable.h"
 
 class lambertian: public material {
 public:
     lambertian(const vec3& a) : albedo(a) {}
 
-    virtual bool scatter(const ray& r_in, const hit_record rec, vec3& attenuation, ray& scattered) const {
-         vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-         scattered = ray(rec.p, target - rec.p);
-         attenuation = albedo;
-         return true;
-    }
+    virtual bool scatter(const ray& r_in, const hit_record rec, vec3& attenuation, ray& scattered) const;
 
     vec3 albedo;
-
 };
 
 
